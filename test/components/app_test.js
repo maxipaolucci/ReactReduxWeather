a5b8ef2005e47b4b9e9958c3659f077994ff104d
Maxi Paolucci mac <maxipaolucci@gmail.com>
2021-03-11 09:36:45 +1300

    [nested-stacks] force test to pass

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -4,9 +4,9 @@ import App from '../../src/components/app';
 describe('App' , () => {
   let component;
 
-  // beforeEach(() => {
-  //   component = renderComponent(App);
-  // });
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
 
   it('renders something', () => {
     expect(component).to.exist;
